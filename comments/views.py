f6c79ff4a48770c8563767fdc689a2f4d43f9c05
Sahil <n.sahil2709@gmail.com>
2026-03-21 12:25:12 +0530

    created views for comments where the user can post comments on that particular post only

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import Comment
+from .serializers import CommentSerializer
 
-# Create your views here.
+
+class CommentListCreateView(generics.ListCreateAPIView):
+  serializer_class   = CommentSerializer
+  permission_classes = [IsAuthenticatedOrReadOnly]
+
+  def get_queryset(self):
+    # Only return comments for the specific post in the URL
+    return Comment.objects.filter(post_id=self.kwargs['post_pk'])
+
+  def perform_create(self, serializer):
+    serializer.save(
+      user=self.request.user,
+      post_id=self.kwargs['post_pk']
+    )
