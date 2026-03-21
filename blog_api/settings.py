4d801e2af697b21b5ea27b97d00ee380b5bb7622
Sahil <n.sahil2709@gmail.com>
2026-03-21 13:33:14 +0530

    feat: added search, filtering, and ordering to posts API Installed django-filter and registered in INSTALLED_APPSand also Added DjangoFilterBackend, SearchFilter, OrderingFilter globally

--- blog_api/settings.py
+++ blog_api/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     # Third-party
     'rest_framework',
     'rest_framework_simplejwt',
+    'django_filters',
 
     # Our apps
     'users',
@@ -118,6 +119,12 @@ REST_FRAMEWORK = {
     
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
+
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',  
+        'rest_framework.filters.SearchFilter',                
+        'rest_framework.filters.OrderingFilter',               
+    ],
 }
 
 import os
