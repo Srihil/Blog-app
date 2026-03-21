daf754fa16ae357063bf446e25e588c76ce75e69
Sahil <n.sahil2709@gmail.com>
2026-03-21 13:16:54 +0530

    feat: add pagination to posts API Configured PageNumberPagination globally in REST_FRAMEWORK settings and Added custom PostPagination class with page_size_query_param support

--- comments/views.py
+++ comments/views.py
@@ -2,11 +2,13 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Comment
 from .serializers import CommentSerializer
+from blog_api.pagination import PostPagination
 
 
 class CommentListCreateView(generics.ListCreateAPIView):
   serializer_class   = CommentSerializer
   permission_classes = [IsAuthenticatedOrReadOnly]
+  pagination_class   = PostPagination
 
   def get_queryset(self):
     # Only return comments for the specific post in the URL
