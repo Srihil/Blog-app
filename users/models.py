60b7573be1b23041cc3b0071902309828189875f
Sahil <n.sahil2709@gmail.com>
2026-03-21 16:10:32 +0530

    fixed a minor bug issue where i added a user before creating a profile but afterwards i added profile too so thats why there was a cinflict

--- users/models.py
+++ users/models.py
@@ -5,11 +5,11 @@ from django.dispatch import receiver
 
 
 class Profile(models.Model):
-  user     = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-  bio      = models.TextField(blank=True, null=True)
-  avatar   = models.ImageField(upload_to='avatars/', blank=True, null=True)
+  user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+  bio = models.TextField(blank=True, null=True)
+  avatar = models.ImageField(upload_to="avatars/", blank=True, null=True)
   location = models.CharField(max_length=100, blank=True, null=True)
-  website  = models.URLField(blank=True, null=True)
+  website = models.URLField(blank=True, null=True)
 
   def __str__(self):
     return f"{self.user.username}'s Profile"
@@ -20,6 +20,8 @@ def create_user_profile(sender, instance, created, **kwargs):
   if created:
     Profile.objects.create(user=instance)
 
+
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
+  Profile.objects.get_or_create(user=instance)
   instance.profile.save()
