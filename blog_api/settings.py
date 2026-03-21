921b86d295f93a65854d5652d5163e72b84b0715
Sahil <n.sahil2709@gmail.com>
2026-03-21 14:51:09 +0530

    connected database with the postgresql and created a super user sahilmain

--- blog_api/settings.py
+++ blog_api/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'blog_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE':   'django.db.backends.postgresql',
+        'NAME':     'blog_api_db',
+        'USER':     'sahil',
+        'PASSWORD': '1234',
+        'HOST':     'localhost',
+        'PORT':     '5432',
     }
 }
 
